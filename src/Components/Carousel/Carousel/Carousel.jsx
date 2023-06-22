5f544e3c16e0b3810d73afbd2c4843c42e889175
Julio Reyes <juliojesusreyes@gmail.com>
2023-06-22 15:43:24 -0400

    Refactorizando

--- src/Components/Carousel/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel/Carousel.jsx
@@ -1,14 +1,53 @@
-import React from "react";
-import SliderJulioFlix from "../Slider/Slider";
+import React, { useEffect, useState } from "react";
+import { styled } from '@mui/system';
+import { ColoresJulioFlix } from "../../../global";
+import Slider from "../Slider/Slider";
+import uuid from "react-uuid";
 
-const Carousel = () => {
+const ContenedorSlider = styled ("section") ({
+
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: "20px",
+    paddingBottom: "20px",
+    backgroundColor: ColoresJulioFlix.fondo,
+    
+});
+
+const SliderJulioFlix = () => {
+
+    const [DatosCategorias, setDatosCategorias] = useState([])
+    useEffect( () => {
+
+        fetch ("http://localhost:3001/Categorias/")
+            .then (response => response.json())
+            .then (data => setDatosCategorias(data))
+            .catch (err => console.log (err))
+
+    }, []);
 
     return (
 
-        <SliderJulioFlix />
+        
+        <ContenedorSlider>
+
+            {DatosCategorias.map ((props) => {
+
+                return <Slider 
+                    key={uuid()}
+                    ColorCategoria={props.ColorCategoria} 
+                    TituloCategoria={props.TituloCategoria} 
+                    SubtituloCategoria={props.SubtituloCategoria}
+                />
+                
+            })};
+
+        </ContenedorSlider>
 
     );
 
+    
+
 };
 
-export default Carousel;
+export default SliderJulioFlix;
--- src/Components/Carousel/Slider/Slider.jsx
+++ src/Components/Carousel/Slider/Slider.jsx
@@ -1,56 +1,111 @@
 import React, { useEffect, useState } from "react";
 import { styled } from '@mui/system';
 import { ColoresJulioFlix } from "../../../global";
-import SliderInterno from "./Interno/SliderInterno";
+import Video from "../VideoCard/VideoCard";
+import "./Slider.css";
 import uuid from "react-uuid";
 
-const ContenedorSlider = styled ("section") ({
+const ContenedorCategoria = styled ("div") ({
 
     display: "flex",
     flexDirection: "column",
-    paddingTop: "20px",
-    paddingBottom: "20px",
+    flexWrap: "wrap",
+    paddingLeft: "40px",
+    paddingRight: "40px",
+    paddingBottom: "30px",
     backgroundColor: ColoresJulioFlix.fondo,
+    justifyContent: "center",
+    alignItems: "start",
     
 });
 
-const SliderJulioFlix = () => {
+const ContenedorTextosCategoria = styled ("div") ({
 
-    const [DatosCategorias, setDatosCategorias] = useState([])
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingBottom: "20px",
+    
+});
+
+const H1Categoria = styled ("h1") ({
+
+    fontFamily: "Roboto",
+    fontWeight: "400",
+    fontSize: "35px",
+    lineHeight: "41px",
+    userSelect: "none",
+    
+});
+
+const SubTexto = styled ("span") ({
+
+    color: "#F5F5F5",
+    fontFamily: "Roboto",
+    fontWeight: "300",
+    fontSize: "18px",
+    lineHeight: "21px",
+    paddingLeft: "11px",
+    userSelect: "none",
+    
+});
+
+const ContenedorVideos = styled ("div") ({
+
+    display: "flex",
+    flexWrap: "nowrap",
+    overflowX: "auto",
+    backgroundColor: ColoresJulioFlix.fondo,
+    gap: "20px",
+    width: "100%",
+    paddingRight: "40px",
+    
+});
+
+
+const Slider = (props) => {
+
+    const [DatosVideos, setDatosVideos] = useState([])
     useEffect( () => {
 
-        fetch ("http://localhost:3001/Categorias/")
+        fetch ("http://localhost:3001/Videos")
             .then (response => response.json())
-            .then (data => setDatosCategorias(data))
+            .then (data => setDatosVideos(data))
             .catch (err => console.log (err))
 
     }, []);
 
     return (
 
-        
-        <ContenedorSlider>
+        <ContenedorCategoria>
+            <ContenedorTextosCategoria>
+                <H1Categoria style={{color: `${props.ColorCategoria}`}}>{props.TituloCategoria}</H1Categoria>
+                <SubTexto key={uuid()}>{props.SubtituloCategoria}</SubTexto>
+            </ContenedorTextosCategoria>
+            <ContenedorVideos>
 
-            {DatosCategorias.map ((props) => {
+            {DatosVideos.map ((props) => {
 
-                return <SliderInterno 
-                    key={uuid()}
-                    ColorCategoria={props.ColorCategoria} 
-                    TituloCategoria={props.TituloCategoria} 
-                    SubtituloCategoria={props.SubtituloCategoria}
-                />
+                return <Video
+
+                    IdVideo={props.IdVideo}
+                    IdCategoria={props.IdCategoria}
+                    TituloVideo={props.TituloVideo}
+                    IdYoutube={props.IdYoutube}
                 
+                />
+
             })};
 
-        </ContenedorSlider>
+
+            </ContenedorVideos>
+        </ContenedorCategoria> 
 
     );
 
-    
-
 };
 
-export default SliderJulioFlix;
+export default Slider;
 
 
 // const SliderJulioFlix = () => {
